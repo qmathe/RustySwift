3d0bcff94b0fd1ea1fa4a09234d1ef6fadc22925
Quentin Mathé <quentin.mathe@gmail.com>
2022-06-12 19:34:24 +0200

    Initial import

---
+++ Shared/RustySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustySwiftApp.swift
+//  Shared
+//
+//  Created by Quentin Mathé on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RustySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
